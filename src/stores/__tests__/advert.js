1dcb738b2d0cf87c89fe6cd024ce302ad1bdf05f
Luca Nils Schmid <me@lucaschmid.net>
2017-01-11 21:09:10 +0100

    tests(advert): write all missing tests for advert

--- src/stores/__tests__/advert.js
+++ src/stores/__tests__/advert.js
@@ -45,10 +45,111 @@ test('addElement should take use the container\'s dimenons if a size is `-1`', t
   t.is(advert.elements[1].size.height, 54)
 })
 
+test('removeElement should remove an element by id', t => {
+  const toolbox = mkToolbox()
+  const advert = mkAdvert(toolbox.templates[0])
+  advert.addElement(newElement())
+  const id = advert.elements[0].id
+  advert.removeElement(id)
+  t.true(advert.elements.length === 0)
+})
+
+test('moveElement should move an element', t => {
+  const toolbox = mkToolbox()
+  const advert = mkAdvert(toolbox.templates[0])
+  const element0 = newElement()
+  const element1 = newElement()
+  advert.addElement(element0)
+  advert.addElement(element1)
+  advert.moveElement(element0.id, 1)
+  t.is(advert.elements[0].id, element1.id)
+  t.is(advert.elements[1].id, element0.id)
+})
+
+test('setWizard should open the wizard with a given element', t => {
+  const toolbox = mkToolbox()
+  const advert = mkAdvert(toolbox.templates[0])
+  advert.addElement(newElement())
+  advert.setWizard(advert.elements[0].id)
+  t.is(advert.wizard, advert.elements[0].id)
+})
+
+test('newElementWizard should open the wizard and add a new element', t => {
+  const toolbox = mkToolbox()
+  const advert = mkAdvert(toolbox.templates[0])
+  const element = newElement()
+  advert.newElementWizard(element)
+  t.is(advert.wizard, element.id)
+  t.is(advert.elements[0].id, element.id)
+})
+
+test('closeWizard should close the wizard', t => {
+  const toolbox = mkToolbox()
+  const advert = mkAdvert(toolbox.templates[0])
+  advert.addElement(newElement())
+  advert.setWizard(advert.elements[0].id)
+  advert.closeWizard()
+  t.is(advert.wizard, null)
+})
+
+test('wizardElement should be the element with the id of `wizard`', t => {
+  const toolbox = mkToolbox()
+  const advert = mkAdvert(toolbox.templates[0])
+  advert.addElement(newElement())
+  advert.setWizard(advert.elements[0].id)
+  t.is(advert.wizardElement.id, advert.elements[0].id)
+})
+
+test('export should return valid JSON', t => {
+  const toolbox = mkToolbox()
+  const advert = mkAdvert(toolbox.templates[0])
+  t.true(typeof JSON.parse(advert.export()) === 'object')
+})
+
+test('export should contain all exportable attributes', t => {
+  const toolbox = mkToolbox()
+  const advert = mkAdvert(toolbox.templates[0])
+  advert.addElement(mkParagraph(advert.template.slots[0].id))
+  advert.addElement(mkParagraph(advert.template.slots[0].id))
+  advert.setWizard(advert.elements[0].id)
+  t.deepEqual(JSON.parse(advert.export()), {
+    template: advert.template.id,
+    wizard: advert.wizard,
+    elements: [
+      {
+        id: advert.elements[0].id,
+        type: advert.elements[0].type,
+        slot: advert.elements[0].slot,
+        data: {
+          text: advert.elements[0].text,
+          fontSize: advert.elements[0].fontSize,
+          lineHeight: advert.elements[0].lineHeight
+        }
+      },
+      {
+        id: advert.elements[1].id,
+        type: advert.elements[1].type,
+        slot: advert.elements[1].slot,
+        data: {
+          text: advert.elements[1].text,
+          fontSize: advert.elements[1].fontSize,
+          lineHeight: advert.elements[1].lineHeight
+        }
+      }
+    ]
+  })
+})
+
 test('sizeInPx should be an adverts size, as defined in the template, in pixels', t => {
   const toolbox = mkToolbox()
   const advert = mkAdvert(toolbox.templates[0])
   t.deepEqual(advert.sizeInPx, { width: 324, height: 348 })
+})
+
+test('viewBox should be an adverts SVG viewbox dimensions', t => {
+  const toolbox = mkToolbox()
+  const advert = mkAdvert(toolbox.templates[0])
+  t.deepEqual(advert.viewBox, '0 0 54 58')
 })
 
 test('valid should show wether or not all values in an adverts element are valid', t => {
