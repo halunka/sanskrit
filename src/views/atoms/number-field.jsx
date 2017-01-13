8868d35e9d67a925632ad132dde1ac71029e85aa
Luca Nils Schmid <me@lucaschmid.net>
2017-01-13 09:21:26 +0100

    fix(number-field.jsx): eventlistener on number field

--- src/views/atoms/number-field.jsx
+++ src/views/atoms/number-field.jsx
@@ -7,7 +7,13 @@ import type { NumberFieldT } from '../../stores/fields/number'
 const onChange = (field) => withValueFromEvent((v) => field.update(Number(v)))
 
 export default observer(function NumberField (field: NumberFieldT) {
+  const handleChange = onChange(field)
   return (
-    <input type='text' onChange={onChange(field)} value={field.value} />
+    <input
+      type='text'
+      onChange={handleChange}
+      onKeyUp={handleChange}
+      value={field.value}
+      />
   )
 })
