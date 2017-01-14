0c9daa77214fd1206c0bbc619021e14264927d0f
Luca Nils Schmid <me@lucaschmid.net>
2017-01-14 19:04:34 +0100

    feat(storage/cache): implement a cache utility

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -4,8 +4,12 @@ import { autorunAsync } from 'mobx'
 export const save = (key: string, value: any): void =>
   localStorage.setItem(key, JSON.stringify(value))
 
-export const load = (key: string): any =>
-  JSON.parse(localStorage.getItem(key))
+export const load = (key: string): any => {
+  const item = localStorage.getItem(key)
+  return item
+    ? JSON.parse(localStorage.getItem(key))
+    : undefined
+}
 
 export const observe = (key, observable, delay = 5000, project = R.invoker('toJs')): void => {
   autorunAsync(() => {
